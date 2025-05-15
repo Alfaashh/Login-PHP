b023e6db0fe105c63f0901e38ad098860398a49d
Al Fathi Ayyash <150584442+Alfaashh@users.noreply.github.com>
2025-05-16 05:16:14 +0700

    Create auth_check.php

---
+++ auth_check.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+//Durasi timeout 30 menit
+$timeout_duration = 1800;
+
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout_duration) {
+    session_unset();
+    session_destroy();
+    header("Location: login.php?timeout=1");
+    exit();
+}
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$_SESSION['last_activity'] = time();
